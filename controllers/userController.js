73a5821e98ce5efab9a1203d6b1f916d8a2a9a7e
chrischen <chrischen96@outlook.com>
2023-06-12 13:01:57 -0700

    controller template

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,26 @@
 const {Users} = require('../models')
 
-const creat = (req, res) => {
+const creat = async(req, res) => {
 
 }
 
-const getAll = (req, res) => {
+// const getAll = async(req, res) => {
     
-}
+// }
 
-const getById = (req, res) => {
-
-}
-
-const getByName = (req, res) => {
+const getById = async(req, res) => {
 
 }
 
-const update = (req, res) => {
+const getByName = async(req, res) => {
 
 }
 
-const deleteNP = (req, res) => {
+const update = async(req, res) => {
+
+}
+
+const deleteUser = async(req, res) => {
 
 }
 
