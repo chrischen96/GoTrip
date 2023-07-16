84cb07721e4c7e5cfacb71a3298712e6546794b1
chrischen <chrischen96@outlook.com>
2023-07-15 20:01:17 -0700

    XC

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv').config()
 //     .connect('mongodb://127.0.0.1:27017/npdb')
 //     .then(() => console.log('connected to mongodb'))
 //     .catch(error => handleError(error))
-
+console.log(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@calski.lyliq7c.mongodb.net/npdb`)
 mongoose
     .connect(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@calski.lyliq7c.mongodb.net/npdb`)
     .then(() => console.log('connected to mongodb'))
